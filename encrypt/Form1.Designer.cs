11d2d0087dfc1b88cb45f701be1401d4cc5e3d3c
Yu-Dai <61172609+Yu-Dai@users.noreply.github.com>
2021-08-09 02:57:20 +0800

    fianal commit
    fianal commit

---
+++ encrypt/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace encrypt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(686, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "choose";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(3, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(315, 361);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("標楷體", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(3, 379);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(212, 49);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "產生KEY";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("標楷體", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.Location = new System.Drawing.Point(819, 381);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(225, 49);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "加密";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(365, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(315, 361);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(767, 14);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(315, 308);
+            this.textBox3.TabIndex = 6;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("標楷體", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button4.Location = new System.Drawing.Point(1107, 381);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(108, 49);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "解密";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(1107, 13);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(315, 361);
+            this.textBox4.TabIndex = 8;
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("標楷體", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button5.Location = new System.Drawing.Point(1235, 381);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(187, 49);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "純解密";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(767, 351);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(315, 21);
+            this.textBox5.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1451, 450);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
+
---
+++ encrypt/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace encrypt
+{
+    public partial class Form1 : Form
+    {
+        private string path = "";
+        private string publicKey = "";
+        private string privateKey = "";
+        private byte[] a = new byte[1280];
+        private string b = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                path = openFileDialog1.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GetKey();
+        }
+
+        // Encrypt a file.
+        public static void AddEncryption(string FileName)
+        {
+
+            File.Encrypt(FileName);
+        }
+
+        // Decrypt a file.
+        public static void RemoveEncryption(string FileName)
+        {
+            File.Decrypt(FileName);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string str = textBox5.Text;
+            a = Encryption(str);
+            textBox3.Text = Convert.ToBase64String(a);
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter("123(Encrypted).txt", false))
+            {
+                file.WriteLine(Convert.ToBase64String(a));
+                //file.WriteLine("\t");
+                //file.WriteLine(Encrypfilecontent);
+            }
+        }
+        
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        /// <summary>
+        /// 獲取加密所使用的key，RSA算法是一種非對稱密碼算法，所謂非對稱，就是指該算法需要一對密鑰，使用其中一個加密，則需要用另一個才能解密。
+        /// </summary>
+        public void GetKey()
+        {
+            string PublicKey = string.Empty;
+            string PrivateKey = string.Empty;
+            RSACryptoServiceProvider rSACryptoServiceProvider = new RSACryptoServiceProvider();
+            PublicKey = rSACryptoServiceProvider.ToXmlString(false); // 獲取公匙，用於加密
+            PrivateKey = rSACryptoServiceProvider.ToXmlString(true); // 獲取公匙和私匙，用於解密
+
+            textBox1.Text = PublicKey;        // 輸出公匙
+            //Console.WriteLine("PrivateKey is {0}", PrivateKey);
+            textBox2.Text = PrivateKey;   // 輸出密匙
+            // 密匙中含有公匙，公匙是根據密匙進行計算得來的。
+
+            using (StreamWriter streamWriter = new StreamWriter("PublicKey.xml"))
+            {
+                streamWriter.Write(rSACryptoServiceProvider.ToXmlString(false));// 將公匙保存到運行目錄下的PublicKey
+            }
+            using (StreamWriter streamWriter = new StreamWriter("PrivateKey.xml"))
+            {
+                streamWriter.Write(rSACryptoServiceProvider.ToXmlString(true)); // 將公匙&私匙保存到運行目錄下的PrivateKey
+            }
+        }
+
+
+        /// <summary>
+        /// 加密
+        /// </summary>
+        /// <param name="str">需要加密的明文</param>
+        /// <returns></returns>
+        private static byte[] Encryption(string str)
+        {
+            RSACryptoServiceProvider rSACryptoServiceProvider = new RSACryptoServiceProvider();
+            using (StreamReader streamReader = new StreamReader("PublicKey.xml")) // 讀取運行目錄下的PublicKey.xml
+            {
+                rSACryptoServiceProvider.FromXmlString(streamReader.ReadToEnd()); // 將公匙載入進RSA實例中
+            }
+            byte[] buffer = Encoding.UTF8.GetBytes(str); // 將明文轉換為byte[]
+
+            // 加密後的數據就是一個byte[] 數組,可以以 文件的形式保存 或 別的形式(網上很多教程,使用Base64進行編碼化保存)
+            byte[] EncryptBuffer = rSACryptoServiceProvider.Encrypt(buffer, false); // 進行加密
+
+            //string EncryptBase64 = Convert.ToBase64String(EncryptBuffer); // 如果使用base64進行明文化，在解密時 需要再次將base64 轉換為byte[]
+            //Console.WriteLine(EncryptBase64);
+            return EncryptBuffer;
+        }
+
+
+        private static string Decrypt(byte[] buffer)
+        {
+            RSACryptoServiceProvider rSACryptoServiceProvider = new RSACryptoServiceProvider();
+            using (StreamReader streamReader = new StreamReader("PrivateKey.xml")) // 讀取運行目錄下的PrivateKey.xml
+            {
+                rSACryptoServiceProvider.FromXmlString(streamReader.ReadToEnd()); // 將私匙載入進RSA實例中
+            }
+            // 解密後得到一個byte[] 數組
+            try
+            {
+                byte[] DecryptBuffer = rSACryptoServiceProvider.Decrypt(buffer, false); // 進行解密
+                string str = Encoding.UTF8.GetString(DecryptBuffer); // 將byte[]轉換為明文
+                return str;
+            }
+            catch {
+                MessageBox.Show("解密失敗");
+                return null;
+            }          
+        }
+
+
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            b = Decrypt(a);
+            textBox4.Text = b;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {   string news = ""; 
+            string c = File.ReadAllText(path);
+            try
+            {
+                byte[] bytes = Convert.FromBase64String(c);
+                news = Decrypt(bytes);
+            }
+            catch
+            {
+                MessageBox.Show("此金鑰已被使用，或無效金鑰");
+            }
+            textBox4.Text = news;
+           
+        }
+    }
+}
